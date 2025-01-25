0d0fb13ac6e853381a77655dd90448d3ff18d35d
3ai4ik <stepanishen.petya@gmail.com>
2025-01-26 00:26:40 +0100

    fix last commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,7 @@
-import Header from "@/components/Header/Header";
-import { NextIntlClientProvider, useTranslations } from "next-intl";
-import { getMessages, getTranslations } from "next-intl/server";
 import "@/app/globals.scss";
-import Footer from "@/components/Footer/Footer";
 import { Assistant, Roboto } from "next/font/google";
 import "@/app/styles/components/error.scss";
+import { getTranslations } from "next-intl/server";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -19,20 +16,15 @@ const assistant = Assistant({
 });
 
 const NotFoundPage = async () => {
-  const messages = await getMessages();
   const t = await getTranslations("Errors");
   return (
     <html>
       <body className={roboto.variable + " " + assistant.variable}>
-        <NextIntlClientProvider messages={messages}>
-          <Header />
-          <main>
-            <div className="error">
-              <h3 className="error__title">{t("notFound") + " :("}</h3>
-            </div>
-          </main>
-          <Footer />
-        </NextIntlClientProvider>
+        <main>
+          <div className="error">
+            <h3 className="error__title">{t("notFound") + " :("}</h3>
+          </div>
+        </main>
       </body>
     </html>
   );

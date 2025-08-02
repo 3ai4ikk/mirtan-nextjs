7d6385e8ce6b09d596ed0077a92a90295f869fa3
3ai4ik <stepanishen.petya@gmail.com>
2025-08-02 14:11:45 +0200

    little fix

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,16 +1,9 @@
-import { type ClassValue, clsx } from "clsx";
-import { revalidatePath } from "next/cache";
-import { twMerge } from "tailwind-merge";
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const revalidate = async (path: string) => {
-  revalidatePath(path, "page");
-};
-
-export const langs = ["ru", "en", "tr"] as const;
 
 export enum Category {
   EMPTY = "Без категории",
